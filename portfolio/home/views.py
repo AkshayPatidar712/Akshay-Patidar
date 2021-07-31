6a160ab4b9490c9444ec6d60bb5038d74fb307e6
Akshay Patidar <akshay.patidar111@gmail.com>
2021-07-31 21:00:19 +0530

    initial commit

---
+++ portfolio/home/views.py
@@ -0,0 +1,21 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
