d3ed50a655f188abd76377449f718c4cd45c0830
Akshay Patidar <akshay.patidar111@gmail.com>
2021-08-01 09:03:48 +0530

    added static files

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -123,17 +123,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    '/var/www/static/'
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-MEDIA_URL = 'media/'
+# MEDIA_URL = 'media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
